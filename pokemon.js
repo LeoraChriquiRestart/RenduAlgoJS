bf914bccc85f6d3eefd7c581bf181949a88dd1cf
Léoratz <127514989+LeoraChriquiRestart@users.noreply.github.com>
2023-10-11 09:12:50 +0200

    Initialisation

---
+++ pokemon.js
@@ -0,0 +1,10 @@
+class Pokemon {
+    constructor(name, attack, defense, hp, luck) {
+        this.name = name
+        this.attack = attack
+        this.defense = defense
+        this.hp = hp
+        this.luck = luck
+    }
+}
+
